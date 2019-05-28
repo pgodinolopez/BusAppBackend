22260d96b7d0f9d148c7be0101d251395d8b8add
pgodino <pedrogod9@gmail.com>
2019-05-29 00:52:15 +0200

    Se crea un metodo para borrar rutas favoritas

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -360,7 +360,7 @@ class ApiController extends FOSRestController {
      * 
      * @SWG\Tag(name="RutaFavorita")
      */
-    public function addCitaAction(Request $request) {
+    public function addRutaFavoritaAction(Request $request) {
         $serializer = $this->get('jms_serializer');
         $em = $this->getDoctrine()->getManager();
         $ruta_favorita = [];
@@ -433,44 +433,20 @@ class ApiController extends FOSRestController {
     }
 
     /**
-     * @Rest\Put("/v1/citas/{id}.{_format}", name="citas_update", defaults={"_format":"json"})
+     * @Rest\Delete("/v1/rutas_favoritas/{id}.{_format}", name="rutas_favoritas_delete", defaults={"_format":"json"})
      *
      * @SWG\Response(
      *     response=201,
-     *     description="Cita was updated successfully"
+     *     description="Ruta favorita was deleted successfully"
      * )
      *
      * @SWG\Response(
      *     response=500,
-     *     description="An error was occurred trying to update cita"
-     * )
-     *
-     * @SWG\Parameter(
-     *     name="dia",
-     *     in="body",
-     *     type="string",
-     *     description="El dia de la cita",
-     *     schema={}
-     * )
-     *  
-     * @SWG\Parameter(
-     *     name="hora",
-     *     in="body",
-     *     type="string",
-     *     description="La hora de la cita",
-     *     schema={}
+     *     description="An error was occurred trying to delete ruta favorita"
      * )
      * 
      * @SWG\Parameter(
-     *     name="observaciones",
-     *     in="body",
-     *     type="string",
-     *     description="Observaciones de la cita",
-     *     schema={}
-     * )
-     * 
-     * @SWG\Parameter(
-     *     name="id",
+     *     name="_id",
      *     in="body",
      *     type="string",
      *     description="El id del usuario",
@@ -479,86 +455,41 @@ class ApiController extends FOSRestController {
      * 
      * @SWG\Tag(name="Cita")
      */
-    public function updateCitaAction(Request $request, $id) {
+    public function deleteRutaFavoritaAction(Request $request, $id) {
         $serializer = $this->get('jms_serializer');
         $em = $this->getDoctrine()->getManager();
-        $cita = [];
+        $ruta_favorita = [];
         $message = "";
 
         try {
             $code = 201;
             $error = false;
-            $cita = $this->getDoctrine()->getRepository(Cita::class)->findOneBy(['id'=>$id]);
-            $dia = $request->request->get("dia", null);
-            $hora = $request->request->get("hora", null);
-            $observaciones = $request->request->get("observaciones", null);
+            $ruta_favorita = $this->getDoctrine()->getRepository(RutaFavorita::class)->findOneBy(['id'=>$id]);
 
-            if (!is_null($dia) && !is_null($hora) && !is_null($observaciones)) {
-                $cita->setDia($dia);
-                $cita->setHora($hora);
-                $cita->setObservaciones($observaciones);
-
-                // $token_dispositivo = 'cXmtEchpxeU:APA91bEj6vmvW6dtGoVjFYp5fiIs9UF3pXucv-jNMv1nB-EWxZvPHqQgu_91mMKVMsGHrP6q36zxdiWwn3rwiEKV76FYNGywuPaYvAJhwKNps193-8-vE9dxuroiElS8uhPZGzrNCBye';
-                // cXmtEchpxeU:APA91bEj6vmvW6dtGoVjFYp5fiIs9UF3pXucv-jNMv1nB-EWxZvPHqQgu_91mMKVMsGHrP6q36zxdiWwn3rwiEKV76FYNGywuPaYvAJhwKNps193-8-vE9dxuroiElS8uhPZGzrNCB
-                
-                // $token_dispositivo = $user->getTokenDispositivo();
-                // $mensaje = 'Nueva cita el dia ' . $dia . ' a las ' . $hora;
-
-                // $this->enviarNotificacionFirebase($token_dispositivo, 'Cita', $mensaje);
-                $em->persist($cita);
+            if (!is_null($ruta_favorita)) {
+                $em->remove($ruta_favorita);
                 $em->flush();
             } else {
                 $code = 500;
                 $error = true;
-                $message = "An error has occurred trying to update cita - Error: You must to provide a cita name";
+                $message = "An error has occurred trying to delete ruta favorita - Error: You must to provide a ruta favorita id";
             }
         } catch (Exception $ex) {
             $code = 500;
             $error = true;
-            $message = "An error has occurred trying to update cita - Error: {$ex->getMessage()}";
+            $message = "An error has occurred trying to delete ruta favorita - Error: {$ex->getMessage()}";
         }
 
         $response = [
             'code' => $code,
             'error' => $error,
-            // 'token' => $token_dispositivo,
-            'data' => $code == 201 ? $cita : $message,
+            'data' => $code == 201 ? $ruta_favorita : $message,
         ];
 
         return new Response($serializer->serialize($response, "json"));
     }
 
     public function enviarNotificacionFirebase($token_dispositivo, $titulo, $mensaje) {
-        // $url = 'https://fcm.googleapis.com/v1/projects/appreservacitas-4944e/messages:send';
-
-        // $fields = array (
-        //         'registration_ids' => array (
-        //                 $token_dispositivo
-        //         ),
-        //         'data' => array (
-        //                 "message" => $mensaje
-        //         )
-        // );
-        // $fields = json_encode ( $fields );
-
-        // $headers = array (
-        //         'Authorization: Bearer ' . 'MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDQF4yk0nKNKr0P\ng7Cn2eRQNrBHskKTlfUl2swV3eDxZUyxNqawxeETMZ5VIfskkw26eXnXoc5aSZLs\nQlMh206f1T+rNNQTaihUrKENxWtPvNn1BrmiBm3OaXNNmZWTIqdEtRU+P6n/ODEM\nk0elDa50RgO/bzOGxv4JKsvJFZvNkZgz9jDvwrobKqWv2XFLeJ8JLU9B3t3axY/f\n5EidYaTWKC4k25C1/Y6taKajHq2nMxxFYQqBJTFtBZZitbrjyt3FgBuUm/1Ng3mL\nF70cGlS0lIZ/X5vb8DbycmLn5rXzFpY5hd/yoD8HZxD2fErxUZf7jdg3N4RaQtQV\nwEZtlTDLAgMBAAECggEAPUa5/S0VW1l6+88RHZRNpYs9JJerAC+31TJVR5kjLKJi\nT4ri0gorCY5ia/pKLI57C+7KgMNecvrYX2b2ePFN+UX/7ifrzV3Ey45cDKSWQZBA\ndGVLE5mmCxLFR9QSlhWtwM88Fq0Dn6qJ0wSpo8JgHVAiuWQC/cyqMMPf53JMA1+0\nzJPSv4fZAcrC8l0EJHjc/8HeT+68CK0bibpqx5cM0uAoGFveW8FA16nwsrlRdww7\nWsfusLdWDuyTXvMXi8aYYnyAPQQNI4frO5BA61HlYGCFciX+a0ZH7exTpPkUbwVc\nzxFU6ZUtDcWzs3LbFLfiJnfGMCLvsNnU13ViMPrl6QKBgQDo6x9ZuTI5OrZQIVhg\nCKFW4SeCMvt/54jnKMQNp+S5dIpGccpkjUreLjVYiZ7YeAFBTmbX6WyOfTWzco0w\nnVxcwA0CUVpFn3wtJ887tC3Fv1fHb5xNWnK/GGDlfC98hko1KTob/EqBvgpd+i9p\nYKLczzeR2nnuV2JEf/IRByHQCQKBgQDktpuX91qovmRrWZAcNIH62D2KcUvHSMa2\n8BnStP6TUGl5dUqJCnOhs1esdcJXpL4Or5fSnBkyixEuT6RwGr9Jk/XD7UzsszU0\nviFz5qLu+1BrzL9dsOXG6QIdVGmLSKBrlzBQMM6eEUKFPq+HKUseBXo1W3xACP+y\n1XS5xQSHMwKBgQCnEeg18pps5IlbZt++WtJnwC8XvDwcXdccgGhIp1JwGIEK0Jpt\nj8/RGPIY5PY0rbewwW0RmJTOjE+VvSg7Y9SZjwSF0hbfc+uddD24xKBEhOCQ5KUH\n80X1fqYaVf614pmeEkllQ42qDMfg6xFRFAkeO+DPVRg69yE8o03eGvCNCQKBgQDF\nFb56c4JCCt7JysHuLCkdmZ1eYUblkYb7OWdnNORy2UYqjbIO6Vy/KKYSTN+NWr8U\nNKflqvHjpgbGG4cdu06+/qs3jJNPHRZqPrThBQu+V/3zBtqYx8kZYMybrZPNnGXw\nkLXnREen2kOyQlRLan/6fMnUlOM64wZEesA9HNNMFQKBgQCXkTBny/suV/av8sG8\nfPLLaeeq/lgPpaWkEg82mHR/CNO5rdaP5YYaYeVaOZzl6v2ikd0r2+YemU+XP+iu\nF/PrR78oOiybIQTZaEw/a8RXeWpKRo4yQPNZmG7ndrYqMd5FJu1mY+EUw5n8zKoR\nNugBdMoevqtrG6S1PgkRK/LlLA==',
-        //         'Content-Type: application/json'
-        // );
-
-        // $ch = curl_init ();
-        // curl_setopt ( $ch, CURLOPT_URL, $url );
-        // curl_setopt ( $ch, CURLOPT_POST, true );
-        // curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
-        // curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
-        // curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
-
-        // $result = curl_exec ( $ch );
-        // echo $result;
-        // curl_close ( $ch );
-
-
-
 
         $url = 'https://fcm.googleapis.com/fcm/send';
 
@@ -586,18 +517,5 @@ class ApiController extends FOSRestController {
         $result = curl_exec($ch);
         echo $result;
         curl_close($ch);
-
-
-
-
-        // curl -X POST -H "Authorization: Bearer ya29.ElqKBGN2Ri_Uz...HnS_uNreA" -H "Content-Type: application/json" -d '{
-        //     "message":{
-        //       "notification": {
-        //         "title": "FCM Message",
-        //         "body": "This is an FCM Message",
-        //       },
-        //       "token": "bk3RNwTe3H0:CI2k_HHwgIpoDKCIZvvDMExUdFQ3P1..."
-        //       }
-        //     }' https://fcm.googleapis.com/v1/projects/myproject-b5ae1/messages:send
     }
 }
